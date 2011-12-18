51ed1073ea1889a1f96c721b2cf938eb5db0614d
Artur Dwornik <Artur.Dwornik@gmail.com>
2011-12-18 23:01:47 +0100

    using semaphore implemenation.

--- Agents/Util/WaitQueue.cs
+++ Agents/Util/WaitQueue.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -9,36 +10,22 @@ namespace Agents.Util
     public class WaitQueue<TValue>
     {
         private readonly NoWaitQueue<TValue> _innerQueue = new NoWaitQueue<TValue>();
-        private ManualResetEvent _event = new ManualResetEvent(false);
+        //private ManualResetEvent _event = new ManualResetEvent(false);
+        private readonly Semaphore _semaphore = new Semaphore(0, int.MaxValue);
         private int _eventFlag = 0;
 
         public void Add(TValue value)
         {
             _innerQueue.Add(value);
-            if (_eventFlag == 1)
-            {
-                _eventFlag = 0;
-                _event.Set();
-            }
+            _semaphore.Release();
         } 
 
         public TValue Take()
         {
-            while (true)
-            {
-                for (int i = 0; i < 3; i++)
-                {
-                    var result = _innerQueue.TakeNoWait();
-                    if (result.Success)
-                    {
-                        return result.Value;
-                    }
-                }
-                _eventFlag = 1;
-                _event.Reset();
-                // timeout is last line of defence if object is inconsistent
-                _event.WaitOne(10000);
-            }
+            _semaphore.WaitOne();
+            var result = _innerQueue.TakeNoWait();
+            if (result.Success) return result.Value;
+            else throw new InvalidAsynchronousStateException("at this point there must be element in queue.");
         }
     }
 }
